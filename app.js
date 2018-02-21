41310f8d40a5be387949c130b282d74a92a0a3d2
Joseph123 <j0sephh@abv.bg>
2018-02-21 11:48:23 +0200

    first

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("Application has started.");
+
+let filterInput = document.getElementById('filterInput');
+filterInput.addEventListener('keyup', filterNames);
+
+function filterNames(){
+  // Get the value of the input
+  let filterValue = document.getElementById('filterInput').value.toUpperCase();
+  console.log("Filter Value: "+filterValue);
+  // Get names ul
+  let ul = document.getElementById('names');
+  // Get li's from ul
+  let li = ul.querySelectorAll('li.collection-item');
+  // Loop through collection-item lis
+  for (let i = 0 ; i < li.length ; i ++ ) {
+    let a = li[i].getElementsByTagName('a')[0];
+    // If matched
+    if(a.innerHTML.toUpperCase().indexOf(filterValue)>-1){
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
